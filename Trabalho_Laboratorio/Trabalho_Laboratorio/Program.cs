56e9f26c988a5dbea94f0126af1a9bfed27b77ca
Francisco Meireles <franciscomeireles-10@hotmail.com>
2020-12-04 11:08:02 +0000

    Base de dados é inicializada automaticamente após a primeira execução do código

--- Trabalho_Laboratorio/Trabalho_Laboratorio/Program.cs
+++ Trabalho_Laboratorio/Trabalho_Laboratorio/Program.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Trabalho_Laboratorio.Data;
 
 namespace Trabalho_Laboratorio
 {
@@ -14,6 +16,26 @@ namespace Trabalho_Laboratorio
 		public static void Main(string[] args)
 		{
 			CreateHostBuilder(args).Build().Run();
+			var host = CreateHostBuilder(args).Build();
+
+			using (var scope = host.Services.CreateScope())
+			{
+				var services = scope.ServiceProvider;
+
+				try
+				{
+					var context = services.GetRequiredService<ApplicationDbContext>();
+					DbInitialiser.Initialize(context);
+				}
+				catch (Exception ex)
+				{
+					var logger = services.GetRequiredService<ILogger<Program>>();
+					logger.LogError(ex, "An error ocurred while seeding the database.");
+					throw;
+				}
+			}
+
+			host.Run();
 		}
 
 		public static IHostBuilder CreateHostBuilder(string[] args) =>
