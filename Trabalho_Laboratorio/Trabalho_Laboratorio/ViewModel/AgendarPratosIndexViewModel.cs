5f5d606bc62d32bc8140ca3b8d69d770db39edff
Francisco Meireles <franciscomeireles-10@hotmail.com>
2021-01-11 18:14:53 +0000

    Commit(11/01/2021) - Morada vísivel na página de detalhes do restaurante - Link funcional para a página de restaurante

---
+++ Trabalho_Laboratorio/Trabalho_Laboratorio/ViewModel/AgendarPratosIndexViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Trabalho_Laboratorio.Helpers;
+using Trabalho_Laboratorio.Models;
+
+namespace Trabalho_Laboratorio.ViewModel
+{
+	public class AgendarPratosIndexViewModel : AgendarPrato
+	{
+		public Int64 Contagem { get; set; }
+
+		public AgendarPratosIndexViewModel(AgendarPrato _prato, int _contagem)
+		{
+			// This copies all the properties from fruit to this instance
+			this.FillProperties(_prato);
+			// Now we can take care of the new properties
+			this.Contagem = _contagem;
+		}
+	}
+}
